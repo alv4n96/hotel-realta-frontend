d3eb7de36db70321c1c470c90492483d83bd4ebf
alvan <alvan.ghon@gmail.com>
2023-04-05 16:34:17 +0700

    make card component for table hotel

--- Realta.Frontend/Components/Hotel/CardHotel.razor.cs
+++ Realta.Frontend/Components/Hotel/CardHotel.razor.cs
@@ -3,7 +3,7 @@ using Realta.Contract.Models.v1.Hotels;
 
 namespace Realta.Frontend.Components.Hotel;
 
-public partial class HotelFacilitiesTable
+public partial class CardHotel
 {
     [Parameter] 
     public HotelsDto HotelData { get; set; }
