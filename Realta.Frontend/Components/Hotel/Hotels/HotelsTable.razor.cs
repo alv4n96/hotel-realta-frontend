37504b1ec59270ad16a77ab3be3d13f94b61cd43
alvan <alvan.ghon@gmail.com>
2023-04-05 16:35:52 +0700

    create logic for hotels table

--- Realta.Frontend/Components/Hotel/Hotels/HotelsTable.razor.cs
+++ Realta.Frontend/Components/Hotel/Hotels/HotelsTable.razor.cs
@@ -3,10 +3,25 @@ using Realta.Contract.Models.v1.Hotels;
 
 namespace Realta.Frontend.Components.Hotel.Hotels
 {
-   public partial class HotelsTable
-   {
-      [Parameter] 
-      public  List<HotelsDto> DataHotel { get; set; }
-      
-   } 
+    public partial class HotelsTable
+    {
+        [Parameter] public List<HotelsDto> DataHotel { get; set; }
+
+        [Parameter]
+        public EventCallback OnAdd { get; set; }
+        
+        [Parameter]
+        public EventCallback OnEdit { get; set; }
+        private void ClearInput()
+        {
+            // panggil event OnAdd ketika tombol "Add" ditekan
+            OnAdd.InvokeAsync();
+        }
+
+        private HotelsDto OnUpdate(HotelsDto data)
+        {
+            OnEdit.InvokeAsync(data);
+            return data;
+        }
+    }
 }
