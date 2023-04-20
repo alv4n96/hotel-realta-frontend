d22d13d8d2e639eb8753ce01332f98e17d7bd600
alvan <alvan.ghon@gmail.com>
2023-04-20 08:00:00 +0000

    fixing implementation for Hotels Repository

--- Realta.Frontend/HttpRepository/Hotel/Hotels/HotelsHttpRepository.cs
+++ Realta.Frontend/HttpRepository/Hotel/Hotels/HotelsHttpRepository.cs
@@ -1,4 +1,6 @@
-﻿using System.Text.Json;
+﻿using System.Net.Http.Json;
+using System.Text;
+using System.Text.Json;
 using Microsoft.AspNetCore.WebUtilities;
 using Realta.Domain.RequestFeatures;
 using Realta.Contract.Models.v1.Hotels;
@@ -53,13 +55,62 @@ namespace Realta.Frontend.HttpRepository.Hotel.Hotels
             return hotels;
         }
 
+        public async Task CreateHotel(HotelsDto formHotelDto)
+        {
+            var content = JsonSerializer.Serialize(formHotelDto);
+            var bodyContent = new StringContent(content, Encoding.UTF8, "application/json");
+            var postResult = await _httpClient.PostAsync("hotels", bodyContent);
+            var postContent = await postResult.Content.ReadAsStringAsync();
+            
+            if (!postResult.IsSuccessStatusCode)
+            {
+                throw new ApplicationException(postContent);
+            }
+        }
+
+        public async Task EditHotel(HotelsDto formHotelDto)
+        {
+            var response = await _httpClient.PutAsJsonAsync($"hotels/{formHotelDto.HotelId}", formHotelDto);
+            var content = await response.Content.ReadAsStringAsync();   
+        
+            if (!response.IsSuccessStatusCode)
+            {
+                throw new ApplicationException(content);
+            }
+        }
+
+        public async Task EditStatusHotel(HotelSwitchDto switchDto)
+        {
+            var response = await _httpClient.PutAsJsonAsync($"hotels/switch/{switchDto.HotelId}", switchDto);
+            var content = await response.Content.ReadAsStringAsync();   
+        
+            if (!response.IsSuccessStatusCode)
+            {
+                throw new ApplicationException(content);
+            }
+        }
+
+        public async Task DeleteHotel(int hotelId)
+        {
+            var url = Path.Combine("hotels", hotelId.ToString());
+
+            var deleteResult = await _httpClient.DeleteAsync(url);
+            var deleteContent = await deleteResult.Content.ReadAsStringAsync();
+
+            if (!deleteResult.IsSuccessStatusCode)
+            {
+                throw new ApplicationException(deleteContent);
+            }
+        }
+
         public async Task<PagingResponse<HotelsDto>> GetHotelPaging(HotelsParameters hotelsParameters)
         {
             var queryStringParam = new Dictionary<string, string>
             {
                 ["pageNumber"] = hotelsParameters.PageNumber.ToString(),
-                ["searchTerm"] = hotelsParameters.SearchTerm == null ? "" : hotelsParameters.SearchTerm
-                // ["orderBy"] = hotelsParameters.OrderBy
+                ["searchTerm"] = hotelsParameters.SearchTerm == null ? "" : hotelsParameters.SearchTerm,
+                ["orderBy"] = hotelsParameters.OrderBy,
+                ["pageSize"] = hotelsParameters.PageSize.ToString()
             };
 
 
