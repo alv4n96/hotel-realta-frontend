e145bcf10b672ea43d2bdedc46f293d767bd07ba
alvan <alvan.ghon@gmail.com>
2023-04-20 08:00:00 +0000

    create Interface for Facilities Repository

--- Realta.Frontend/HttpRepository/Hotel/Facilities/IFacilitiesHttpRepository.cs
+++ Realta.Frontend/HttpRepository/Hotel/Facilities/IFacilitiesHttpRepository.cs
@@ -2,6 +2,7 @@
 using Realta.Contract.Models.v1;
 using Realta.Contract.Models.v1.Facilities;
 using Realta.Contract.Models.v1.Hotels;
+using Realta.Domain.Entities;
 using Realta.Domain.RequestFeatures.HotelParameters;
 using Realta.Frontend.Features;
 
@@ -10,6 +11,12 @@ namespace Realta.Frontend.HttpRepository.Hotel.Facilities
     public interface IFacilitiesHttpRepository
     {
         Task<List<FacilitiesDto>> GetFacilities(int hotelId);
+        Task<FacilitiesDto?> GetFacilityById(int hotelId, int faciId);
+        Task CreateFacility(FacilitiesDto formFaciDto);
+        Task EditFacility(FacilitiesDto formFaciDto);
+        Task DeleteFacility(int hotelId, int faciId);
         Task<PagingResponse<FacilitiesDto>> GetFacilitiesPaging(FacilitiesParameters facilitiesParameter, int hotelId);
+        Task<List<CategoryGroupDto>> GetCategoryGroup();
+
     }
 }
