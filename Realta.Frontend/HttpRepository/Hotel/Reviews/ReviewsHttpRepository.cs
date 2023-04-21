360786ad0cb3dc83d5385ed6bc7e0d58bb3a50b6
alvan <alvan.ghon@gmail.com>
2023-04-21 08:00:00 +0000

    fixing implementation logic for reviews Repository

--- Realta.Frontend/HttpRepository/Hotel/Reviews/ReviewsHttpRepository.cs
+++ Realta.Frontend/HttpRepository/Hotel/Reviews/ReviewsHttpRepository.cs
@@ -1,4 +1,5 @@
-﻿using System.Text.Json;
+﻿using System.Text;
+using System.Text.Json;
 using Microsoft.AspNetCore.WebUtilities;
 using Realta.Contract.Models.v1;
 using Realta.Contract.Models.v1.Facilities;
@@ -7,52 +8,52 @@ using Realta.Domain.RequestFeatures;
 using Realta.Domain.RequestFeatures.HotelParameters;
 using Realta.Frontend.Features;
 
-namespace Realta.Frontend.HttpRepository.Hotel.Facilities
+namespace Realta.Frontend.HttpRepository.Hotel.Reviews
 {
-    public class FacilitiesHttpRepository : IFacilitiesHttpRepository
+    public class ReviewsHttpRepository : IReviewsHttpRepository
     {
         private readonly HttpClient _httpClient;
         private readonly JsonSerializerOptions _options;
 
 
-        public FacilitiesHttpRepository(HttpClient httpClient)
+        public ReviewsHttpRepository(HttpClient httpClient)
         {
             _httpClient = httpClient;
             _options = new JsonSerializerOptions { PropertyNameCaseInsensitive = true };
         }
 
-        public async Task<List<FacilitiesDto>> GetFacilities(int hotelId)
+
+        public async Task<List<HotelReviewsDto>> GetReviews(int hotelId)
         {
+
             //Call api end point, e.g :https://localhost:7068/api/hotels 
-            var response = await _httpClient.GetAsync($"facilities/{hotelId}/facilities");
-            var content = await response.Content.ReadAsStringAsync();
+                var response = await _httpClient.GetAsync($"HotelReviews/{hotelId}/reviews/");
+                var content = await response.Content.ReadAsStringAsync();
 
 
-            if (!response.IsSuccessStatusCode)
-            {
-                var resErr = new List<FacilitiesDto>();
-                return resErr;
-            }
+                if (!response.IsSuccessStatusCode)
+                {
+                    var resErr = new List<HotelReviewsDto>();
+                    return resErr;
+                }
 
-            var facilities = JsonSerializer.Deserialize<List<FacilitiesDto>>(content, _options);
+                var facilities = JsonSerializer.Deserialize<List<HotelReviewsDto>>(content, _options);
 
 
-            return facilities;
+                return facilities;
         }
 
-
-        public async Task<PagingResponse<FacilitiesDto>> GetFacilitiesPaging(FacilitiesParameters facilitiesParameter,int hotelId)
+        public async Task<PagingResponse<HotelReviewsDto>> GetReviewsPaging(ReviewsParameters reviewsParameters, int hotelId)
         {
-            
             var queryStringParam = new Dictionary<string, string>
             {
-                ["pageNumber"] = facilitiesParameter.PageNumber.ToString(),
-                ["searchTerm"] = facilitiesParameter.SearchTerm == null ? "" : facilitiesParameter.SearchTerm
-                // ["orderBy"] = facilitiesParameter.OrderBy
+                ["pageNumber"] = reviewsParameters.PageNumber.ToString(),
+                ["searchTerm"] = reviewsParameters.SearchTerm == null ? "" : reviewsParameters.SearchTerm
+                // ["orderBy"] = reviewsParameters.OrderBy
             };
 
 
-            var response = await _httpClient.GetAsync(QueryHelpers.AddQueryString($"facilities/{hotelId}/facilities/pageList",queryStringParam));
+            var response = await _httpClient.GetAsync(QueryHelpers.AddQueryString($"HotelReviews/{hotelId}/reviews/pagelist",queryStringParam));
             var content = await response.Content.ReadAsStringAsync();
 
             if (!response.IsSuccessStatusCode)
@@ -60,44 +61,42 @@ namespace Realta.Frontend.HttpRepository.Hotel.Facilities
                 throw new ApplicationException(content);
             }
 
-            var pagingResponse = new PagingResponse<FacilitiesDto>
+            var pagingResponse = new PagingResponse<HotelReviewsDto>
             {
-                Items = JsonSerializer.Deserialize<List<FacilitiesDto>>(content, _options),
+                Items = JsonSerializer.Deserialize<List<HotelReviewsDto>>(content, _options),
                 MetaData = JsonSerializer.Deserialize<MetaData>(response.Headers.GetValues("X-Pagination").First(), _options)
             };
 
             return pagingResponse;
         }
 
-
-        /*--
-        public Task<PagingResponse<FacilitiesDto>> GetFacilitiesPaging(FacilitiesParameter facilitiesParameter)
+        public async Task CreateReview(HotelReviewsDto formHotelDto)
         {
-        
-            var queryStringParam = new Dictionary<string, string>
-            {
-                ["pageNumber"] = facilitiesParameter.PageNumber.ToString(),
-                ["searchTerm"] = facilitiesParameter.SearchTerm == null ? "" : facilitiesParameter.SearchTerm
-                // ["orderBy"] = facilitiesParameter.OrderBy
-            };
-
-
-            var response = await _httpClient.GetAsync(QueryHelpers.AddQueryString("facilities/{:id}/facilities/pageList",queryStringParam));
-            var content = await response.Content.ReadAsStringAsync();
-
-            if (!response.IsSuccessStatusCode)
-            {
-                throw new ApplicationException(content);
-            }
-
-            var pagingResponse = new PagingResponse<HotelsDto>
-            {
-                Items = JsonSerializer.Deserialize<List<HotelsDto>>(content, _options),
-                MetaData = JsonSerializer.Deserialize<MetaData>(response.Headers.GetValues("X-Pagination").First(), _options)
-            };
-
-            return pagingResponse;
+             var content = JsonSerializer.Serialize(formHotelDto);
+                        var bodyContent = new StringContent(content, Encoding.UTF8, "application/json");
+                        var postResult = await _httpClient.PostAsync($"HotelReviews/{formHotelDto.HoreHotelId}/reviews/", bodyContent);
+                        var postContent = await postResult.Content.ReadAsStringAsync();
+                        
+                        if (!postResult.IsSuccessStatusCode)
+                        {
+                            throw new ApplicationException(postContent);
+                        }
         }
-        --*/
+
+        public async Task DeleteHotel(int hotelId, int horeId)
+        {
+            var url = Path.Combine($"HotelReviews/{hotelId}/reviews/{horeId}");
+
+            var deleteResult = await _httpClient.DeleteAsync(url);
+            var deleteContent = await deleteResult.Content.ReadAsStringAsync();
+
+            if (!deleteResult.IsSuccessStatusCode)
+            {
+                throw new ApplicationException(deleteContent);
+            }
+        }
+
+
+
     }
 }
